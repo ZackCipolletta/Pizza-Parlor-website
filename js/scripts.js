5f1f3c209e2276397d880e94e137de1fcc2b0003
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-27 11:25:45 -0800

    add several functions to test programfor different variables

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,65 @@ Pizza.prototype.addPrice = function(price) {
   this.total += price;
 }
 
+
+
+
+
+
+// UI logic
+
+
+function smallPizza() {
+  myPizza.addSize('small');
+  myPizza.addPrice(12);
+}
+
+function mediumPizza() {
+  myPizza.addSize('medium');
+  myPizza.addPrice(14);
+}
+
+function largePizza() {
+  myPizza.addSize('large');
+  myPizza.addPrice(16);
+}
+
+function extraCheese() {
+  myPizza.addToppings('Extra cheese')
+  myPizza.addPrice(.50);
+}
+
+function pepperoni() {
+  myPizza.addToppings('pepperoni')
+  myPizza.addPrice(1);
+}
+
+function artichoke() {
+  myPizza.addToppings('artichoke')
+  myPizza.addPrice(.50);
+}
+
+function anchovy() {
+  myPizza.addToppings('anchovy')
+  myPizza.addPrice(1);
+}
+
+function mushroom() {
+  myPizza.addToppings('mushroom')
+  myPizza.addPrice(.50);
+}
+
+function sausage() {
+  myPizza.addToppings('sausage')
+  myPizza.addPrice(1);
+}
+
+function peppers() {
+  myPizza.addToppings('peppers')
+  myPizza.addPrice(.50);
+}
+
+function bacon() {
+  myPizza.addToppings('bacon')
+  myPizza.addPrice(1);
+}
